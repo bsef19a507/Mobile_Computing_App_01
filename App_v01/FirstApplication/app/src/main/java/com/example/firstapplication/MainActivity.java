f9b698af18316ba4fce2007e342ee18f56731e30
redmoon <bsef19a507@pucit.edu.pk>
2022-04-16 23:46:28 +0500

    App version 0.1

---
+++ App_v01/FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void showNow(View view){
+        Toast.makeText(this, "Welcome to my First App...!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
