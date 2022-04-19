b9a1eea970f7a4b7a8765bef53fd663894a05ec1
redmoon <bsef19a507@pucit.edu.pk>
2022-04-20 04:48:11 +0500

    App version 0.3 (Tasbeeh)

--- App_v01/FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
+++ App_v01/FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -4,12 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void showNow(View view){
-        Toast.makeText(this, "Welcome to my First App...!", Toast.LENGTH_SHORT).show();
+    Integer i = 0;
+
+    public void resetCount(View view){
+         i = 0;
+        String c =    i.toString();
+        TextView count = (TextView) findViewById(R.id.textView2);
+        count.setText(c);
+    }
+
+    public void countTasbeeh(View view){
+       // Toast.makeText(this, "w", Toast.LENGTH_SHORT).show();
+        i++;
+        String c =    i.toString();
+        TextView count = (TextView) findViewById(R.id.textView2);
+        count.setText(c);
+
     }
 
     @Override
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Mobile_Computing_App_01
-# This is My First Android App
-![Semantic description of image](/App_v01/Screenshot/AppImage.jpeg "Image Title")
+# This is My First Android App version 0.1
+![Semantic description of image](/App_v01/Screenshot/appv0.jpg "Image Title")
+
+# This is My First Android App version 0.2
+![Semantic description of image](/App_v01/Screenshot/appv1.jpg "Image Title")
+
+# This is My First Android App version 0.3 (Tasbeeh)
+![Semantic description of image](/App_v01/Screenshot/appv2.jpg "Image Title")
