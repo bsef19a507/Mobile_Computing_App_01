fd88c45690f697e377337164757379bf482c26d7
redmoon <bsef19a507@pucit.edu.pk>
2022-05-23 18:26:23 +0500

    App version 0.4

--- App_v01/FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
+++ App_v01/FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -4,27 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Integer i = 0;
-
-    public void resetCount(View view){
-         i = 0;
-        String c =    i.toString();
-        TextView count = (TextView) findViewById(R.id.textView2);
-        count.setText(c);
-    }
-
-    public void countTasbeeh(View view){
-       // Toast.makeText(this, "w", Toast.LENGTH_SHORT).show();
-        i++;
-        String c =    i.toString();
-        TextView count = (TextView) findViewById(R.id.textView2);
-        count.setText(c);
-
+    public void showNow(View view){
+        Toast.makeText(this, "Welcome to my First App...!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
